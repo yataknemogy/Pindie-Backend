58d72ef7a1f3087273322de32907a5f7aa8c4aec
yataknemogy <efim@yataknemogy.com>
2024-05-19 23:56:06 +0300

    fix

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 const path = require("path");
 const users = require("../models/user");
+
 const login = (req, res) => {
   const { email, password } = req.body;
   users
