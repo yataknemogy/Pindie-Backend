e05c7945411d1944b3892f126b666f79b1a5efc1
yataknemogy <efim@yataknemogy.com>
2024-05-22 15:55:22 +0400

    Add files via upload

---
+++ middlewares/cors.js
@@ -0,0 +1,14 @@
+const allowedCors = [
+  "https://practicum.yandex.ru",
+  "https://students-projects.ru",
+  "localhost:3000",
+];
+function cors(req, res, next) {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  next();
+}
+
+module.exports = cors;
